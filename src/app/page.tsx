177b3e500ac9aa60b3b6a9640581d0b66a9c8f0b
Zolastic <honhattienlul@gmail.com>
2024-04-01 20:31:16 +0800

    style: gave homepage button a link variant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center space-y-[8px] p-24">
       <Link href="/with-no-drag-trigger">
-        <Button>Drag and Drop without drag trigger</Button>
+        <Button variant="link">Drag and Drop without drag trigger</Button>
       </Link>
       <Link href="/with-drag-trigger">
-        <Button>Drag and Drop with drag trigger</Button>
+        <Button variant="link">Drag and Drop with drag trigger</Button>
       </Link>
     </main>
   );
